78add228f6dcfbe94f76d461bd730ec5474ec1af
Max Pavlov <max@radacode.com>
2018-09-09 23:54:28 +0300

    Adding params parser lib and demo class

--- KookooReplace/Program.cs
+++ KookooReplace/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ICSharpCode.SharpZipLib.Core;
+using CommandLine;
 using ICSharpCode.SharpZipLib.Zip;
 using ZetaLongPaths;
 
@@ -12,6 +13,27 @@ namespace KookooReplace
 {
     class Program
     {
+
+        class Options
+        {
+            [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
+            public IEnumerable<string> InputFiles { get; set; }
+
+            // Omitting long name, defaults to name of property, ie "--verbose"
+            [Option(
+              Default = false,
+              HelpText = "Prints all messages to standard output.")]
+            public bool Verbose { get; set; }
+
+            [Option("stdin",
+              Default = false,
+              HelpText = "Read from stdin")]
+            public bool stdin { get; set; }
+
+            [Value(0, MetaName = "offset", HelpText = "File offset.")]
+            public long? Offset { get; set; }
+        }
+
         static void Main(string[] args)
         {
             var fileNameOfPayload = String.Empty;
